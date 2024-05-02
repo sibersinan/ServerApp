47bcd60d7ce7926ebc27a5b2141d1cb83b5241c0
egazeloglu@gmail.com <egazeloglu@gmail.com>
2024-05-02 15:33:53 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ServerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(341, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 24);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(323, 304);
+            this.txtLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 338);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Mesaj Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerApp
+{
+    public partial class Form1 : Form
+    {
+        private Socket serverSocket;
+        private Socket clientSocket;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Sunucu soketi oluştur
+                IPAddress localAddress = IPAddress.Parse("192.168.0.103");
+                IPEndPoint localEndPoint = new IPEndPoint(localAddress, 12345);
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // İstemciden gelen bağlantıları dinle
+                serverSocket.Bind(localEndPoint);
+                serverSocket.Listen(10);
+
+                // İstemciden gelen bağlantıyı kabul et
+                DisplayMessage("Bağlantı bekleniyor...");
+                clientSocket = serverSocket.Accept();
+                DisplayMessage("İstemci bağlandı.");
+
+                // Veriyi al
+                byte[] data = new byte[1024];
+                int receivedBytes = clientSocket.Receive(data);
+                string message = Encoding.ASCII.GetString(data, 0, receivedBytes);
+                DisplayMessage("Gelen mesaj: " + message);
+
+                // Soketi kapat
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+                serverSocket.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void DisplayMessage(string message)
+        {
+            // TextBox kontrolüne mesajı ekleyerek ekranda göster
+            txtLog.AppendText(message + Environment.NewLine);
+        }
+    }
+}
